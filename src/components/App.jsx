78b8d4832f1fcff3dfc64e64381bfcea9279061d
ira089 <gvir2015@gmail.com>
2023-12-31 15:51:52 +0200

    react_HW2_feedback_fin

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,101 @@
-import Feedback from './Feedback/Feedback';
-// state = {
-//   good: 0,
-//   neutral: 0,
-//   bad: 0,
-// };
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        padding: 18,
+// import Feedback from './Feedback/Feedback';
+import { Component } from 'react';
+import styles from './Feedback/feedback.module.css';
+import Section from './Feedback/Section';
+import Notification from './Feedback/Notification';
+import FeedbackBtn from './Feedback/FeedbackBtn';
+import FeedbackStatistic from './Feedback/FeedbacksStatistic';
 
-        // display: 'flex',
-        // justifyContent: 'center',
-        // alignItems: 'center',
-        // fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      {/* React homework template */}
-      <Feedback />
-    </div>
-  );
-};
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  leaveFeedback = ev => {
+    // console.log(ev.target.name);
+    const counterStatItem = ev.target.name;
+    console.log(counterStatItem);
+    this.setState(prevState => {
+      // console.log(prevState);
+      return { [counterStatItem]: (prevState[counterStatItem] += 1) };
+    });
+  };
+
+  countTotalFeedback() {
+    const statValues = Object.values(this.state);
+    // console.log(statValues);
+    const totalStat = statValues.reduce((total, value) => total + value, 0);
+    return totalStat;
+  }
+
+  countPositiveFeedbackPercentage() {
+    const total = this.countTotalFeedback();
+    if (!total) {
+      return 0;
+    }
+    const positiveFeedback = this.state.good;
+    console.log(positiveFeedback);
+    return Number(((positiveFeedback / total) * 100).toFixed(2));
+  }
+
+  // const values = Object.values(book);
+  nofeedback() {
+    const valuesArr = Object.values(this.state);
+    console.log(valuesArr);
+    const positiveValues = valuesArr.filter(value => value > 0);
+    console.log(positiveValues);
+    return positiveValues.length;
+  }
+
+  render() {
+    const totalStatistic = this.countTotalFeedback();
+    const positiveFeedbackPercentage = this.countPositiveFeedbackPercentage();
+    return (
+      <div className={styles.wrap}>
+        <Section title="Please leave feedback">
+          <div className={styles.blockBtn}>
+            <FeedbackBtn
+              items={this.state}
+              leaveFeedback={this.leaveFeedback}
+            />
+          </div>
+        </Section>
+        {this.nofeedback() ? (
+          <Section title="Statistics">
+            <FeedbackStatistic
+              items={this.state}
+              total={totalStatistic}
+              positiveFeedbackPercentage={positiveFeedbackPercentage}
+            />
+          </Section>
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </div>
+    );
+  }
+}
+
+// const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         padding: 18,
+
+// justifyContent: 'center',
+// alignItems: 'center',
+// fontSize: 40,
+//     color: '#010101',
+//   }}
+// >
+
+/* React homework template */
+
+//       <Feedback />
+//     </div>
+//   );
+// };
+export default App;
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,74 +1,78 @@
-import { Component } from 'react';
-import styles from './feedback.module.css';
-import FeedbackBtn from './FeedbackBtn';
-import FeedbackStatistic from './FeedbacksStatistic';
+// import { Component } from 'react';
+// import styles from './feedback.module.css';
+// import FeedbackBtn from './FeedbackBtn';
+// import FeedbackStatistic from './FeedbacksStatistic';
 
-class Feedback extends Component {
-  // static BtnOptions = ['good', 'neutral', 'bad'];
+// class Feedback extends Component {
+// static BtnOptions = ['good', 'neutral', 'bad'];
 
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+// state = {
+//   good: 0,
+//   neutral: 0,
+//   bad: 0,
+// };
 
-  leaveFeedback = ev => {
-    // console.log(ev.target.name);
-    const counterStatItem = ev.target.name;
-    console.log(counterStatItem);
-    this.setState(prevState => {
-      // console.log(prevState);
-      return { [counterStatItem]: (prevState[counterStatItem] += 1) };
-    });
-  };
+// leaveFeedback = ev => {
+// console.log(ev.target.name);
+// const counterStatItem = ev.target.name;
+// console.log(counterStatItem);
+// this.setState(prevState => {
+// console.log(prevState);
+//     return { [counterStatItem]: (prevState[counterStatItem] += 1) };
+//   });
+// };
 
-  countTotalFeedback() {
-    const statValues = Object.values(this.state);
-    // console.log(statValues);
-    const totalStat = statValues.reduce((total, value) => total + value, 0);
-    return totalStat;
-  }
+// countTotalFeedback() {
+//   const statValues = Object.values(this.state);
+// console.log(statValues);
+//   const totalStat = statValues.reduce((total, value) => total + value, 0);
+//   return totalStat;
+// }
 
-  countPositiveFeedbackPercentage() {
-    const total = this.countTotalFeedback();
-    if (!total) {
-      return 0;
-    }
-    const positiveFeedback = this.state.good;
-    console.log(positiveFeedback);
-    return Number(((positiveFeedback / total) * 100).toFixed(2));
-  }
+// countPositiveFeedbackPercentage() {
+//   const total = this.countTotalFeedback();
+//   if (!total) {
+//     return 0;
+//   }
+//   const positiveFeedback = this.state.good;
+//   console.log(positiveFeedback);
+//   return Number(((positiveFeedback / total) * 100).toFixed(2));
+// }
 
-  render() {
-    const totalStatistic = this.countTotalFeedback();
-    const positiveFeedbackPercentage = this.countPositiveFeedbackPercentage();
-    return (
-      <div>
-        <div className={styles.blockFeedback}>
-          <h2 className={styles.title}>Please leave feedback</h2>
-          <div className={styles.blockBtn}>
-            <FeedbackBtn
-              items={this.state}
-              leaveFeedback={this.leaveFeedback}
-            />
-            {/* <button>Good</button>
-            <button>Bad</button> */}
-          </div>
-        </div>
-        <div className={styles.blockStatistics}>
-          <h2 className={styles.title}>Statistics</h2>
+// render() {
+//   const totalStatistic = this.countTotalFeedback();
+//   const positiveFeedbackPercentage = this.countPositiveFeedbackPercentage();
+//   return (
+//     <div>
+//       <div className={styles.blockFeedback}>
+//         <h2 className={styles.title}>Please leave feedback</h2>
+//         <div className={styles.blockBtn}>
+//           <FeedbackBtn
+//             items={this.state}
+//             leaveFeedback={this.leaveFeedback}
+//           />
 
-          <FeedbackStatistic
-            items={this.state}
-            total={totalStatistic}
-            positiveFeedbackPercentage={positiveFeedbackPercentage}
-          />
-          {/* <li>3</li>
-            <li>4</li> */}
-        </div>
-      </div>
-    );
-  }
-}
+/* <button>Good</button>
+            <button>Bad</button> */
 
-export default Feedback;
+//   </div>
+// </div>
+// <div className={styles.blockStatistics}>
+//   <h2 className={styles.title}>Statistics</h2>
+
+//   <FeedbackStatistic
+//     items={this.state}
+//     total={totalStatistic}
+//     positiveFeedbackPercentage={positiveFeedbackPercentage}
+//   />
+
+/* <li>3</li>
+            <li>4</li> */
+
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Feedback;
